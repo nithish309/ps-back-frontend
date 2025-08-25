8bc97f1281fe266e106718628ad3ded3e2d5cb07
nithish309 <148962856+nithish309@users.noreply.github.com>
2025-08-25 20:10:55 +0530

    Update fetchproducts App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,18 +18,18 @@ function ProductList() {
   };
 
   // Function to fetch products using Fetch API (as a fallback)
-  const fetchProductsWithFetch = () => {
-    fetch('https://backendpanishop.vercel.app/products', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        // Add 'Authorization': 'Bearer YOUR_TOKEN' if needed
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error('Error:', error));
-  };
+  // const fetchProductsWithFetch = () => {
+  //   fetch('https://backendpanishop.vercel.app/products', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       // Add 'Authorization': 'Bearer YOUR_TOKEN' if needed
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => setProducts(data))
+  //     .catch((error) => console.error('Error:', error));
+  // };
 
   // Use effect to load products when the component mounts
   useEffect(() => {
