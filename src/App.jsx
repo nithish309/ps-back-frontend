d962e3d2c4ac9a4e365632d1ebee06b12f88f830
nithish309 <148962856+nithish309@users.noreply.github.com>
2025-08-25 20:34:29 +0530

    Update image App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import axios from 'axios';
 import './App.css'; // Assuming you save styles in a separate CSS file
 
